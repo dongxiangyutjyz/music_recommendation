e2efa81a301ceac9f8558ce925a01a4006f0d44f
James <xdong2@nd.edu>
2018-04-22 15:38:49 -0400

    generate_name

---
+++ generate_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2.7
+import names
+
+def gen_names(num):
+    allnames = []
+    for i in range(num):
+        allnames.append(str(names.get_full_name()))
+
+    return allnames
+
+if __name__ == "__main__":
+    allnames = gen_names(100)
+    print allnames
