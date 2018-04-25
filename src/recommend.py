2f697e36fe9d3c4c6ae3e501ae94c98633aeef8a
Ruochen (Jason) Sun <rsun2@nd.edu>
2018-04-25 19:53:27 -0400

    完成 算法

--- src/recommend.py
+++ src/recommend.py
@@ -1,1 +1,44 @@
-def recommend(user):
+from _music_database import _music_database
+
+def recommend_user(user, mdb):
+    d = mdb.get_user_musiclist(user)
+    if d["status"] == "error":
+        return d
+    else: direct_music_list = d.get(user)
+
+    recommend_rank = {}
+
+    for s in direct_music_list:
+        for u in mdb.musics.get(s):
+            if u == user: continue
+            for ss in mdb.users.get(u):
+                if recommend_rank.has_key(ss):
+                    recommend_rank[ss] += 1
+                else:
+                    recommend_rank[ss] = 1
+
+    top_10 = {}
+    for r in sorted(recommend_rank, key=recommend_rank.get, reverse=True):
+        if r > 10: break
+        top_10[r] = recommend_rank[r]
+        i+= 1
+
+    return top_10
+
+def recommend_music(music, m):
+
+    recommend_rank = {}
+    for u in mdb.musics.get(music):
+        for ss in mdb.users.get(u):
+            if recommend_rank.has_key(ss):
+                recommend_rank[ss] += 1
+            else:
+                recommend_rank[ss] = 1
+
+    top_10 = {}
+    for r in sorted(recommend_rank, key=recommend_rank.get, reverse=True):
+        if r > 10: break
+        top_10[r] = recommend_rank[r]
+        i+= 1
+
+    return top_10
